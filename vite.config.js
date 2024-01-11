b1ad9d4a3324411c77476646c0f45c7ba31d5c76
Shaquille Mandy <shaqmandy@gmail.com>
2024-01-11 18:52:21 -0300

    updated gitconfig

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/block27B_bank_account/'
+  base: '/block27B_bank_account_redux_toolkit/'
 })
